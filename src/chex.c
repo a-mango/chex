4d181a4b2256ec737688d9fe82afbedb8abc3dc4
acm <a-mango@users.noreply.github.com>
2023-05-06 21:19:27 +0200

    feat: add build version and print demo

--- src/chex.c
+++ src/chex.c
@@ -1,9 +1,19 @@
+#include <locale.h>
 #include <stdio.h>
+
+#include "board.h"
+#include "util.h"
+
 
 #define CHEX_VERSION "0.0.1"
 
 int main(void) {
-    printf("Chex Chess Engine\n");
-    printf("Build: "CHEX_VERSION"\n");
+    setlocale(LC_ALL, "en_US.UTF-8");
+    cx_log("Chex Chess Engine", CX_LOG_INFO);
+    cx_log("Build: "CHEX_VERSION, CX_LOG_INFO);
+
+    cx_board_t *board = cx_board_init();
+    cx_board_print(board);
+
     return 0;
 }
