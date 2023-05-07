0ffcea7039c35533db26fdfd3d7ab5410e898efa
acm <a-mango@users.noreply.github.com>
2023-05-07 14:04:19 +0200

    feat: add bitboard ordering enum

--- src/board.h
+++ src/board.h
@@ -25,38 +25,57 @@ typedef struct {
     uint8_t  halfmove_clock;
     uint8_t  fullmove_number;
 
-    cx_bitboard_t white_pawns;
-    cx_bitboard_t white_knights;
-    cx_bitboard_t white_bishops;
-    cx_bitboard_t white_rooks;
-    cx_bitboard_t white_queens;
-    cx_bitboard_t white_king;
-
-    cx_bitboard_t black_pawns;
-    cx_bitboard_t black_knights;
-    cx_bitboard_t black_bishops;
-    cx_bitboard_t black_rooks;
-    cx_bitboard_t black_queens;
-    cx_bitboard_t black_king;
+    cx_bitboard_t pieces[16];
 } cx_board_t;
 
-enum {
-    CX_EMPTY,
-    CX_WHITE_PAWN,
-    CX_WHITE_KNIGHT,
-    CX_WHITE_BISHOP,
-    CX_WHITE_ROOK,
-    CX_WHITE_QUEEN,
-    CX_WHITE_KING,
-    CX_BLACK_PAWN,
-    CX_BLACK_KNIGHT,
-    CX_BLACK_BISHOP,
-    CX_BLACK_ROOK,
-    CX_BLACK_QUEEN,
-    CX_BLACK_KING,
+enum CxBitboardOrder {
+    CX_BB_EMPTY,
+    CX_BB_WHITE_PAWNS,
+    CX_BB_WHITE_KNIGHTS,
+    CX_BB_WHITE_BISHOPS,
+    CX_BB_WHITE_ROOKS,
+    CX_BB_WHITE_QUEENS,
+    CX_BB_WHITE_KING,
+    CX_BB_BLACK_PAWNS,
+    CX_BB_BLACK_KNIGHTS,
+    CX_BB_BLACK_BISHOPS,
+    CX_BB_BLACK_ROOKS,
+    CX_BB_BLACK_QUEENS,
+    CX_BB_BLACK_KING,
+    CX_BB_WHITE,
+    CX_BB_BLACK,
+    CX_BB_ALL,
 };
 
-enum { CX_COLOR_WHITE, CX_COLOR_BLACK };
+enum CxPieceType {
+    CX_PAWN = 1,
+    CX_KNIGHT,
+    CX_BISHOP,
+    CX_ROOK,
+    CX_QUEEN,
+    CX_KING,
+};
+
+enum CxColor {
+    CX_WHITE = CX_BIT << 3,
+    CX_BLACK = CX_BIT << 4
+};
+
+enum CxPiece {
+    CX_EMPTY = 0,
+    CX_WHITE_PAWN = (CX_WHITE | CX_PAWN),
+    CX_WHITE_KNIGHT = (CX_WHITE | CX_KNIGHT),
+    CX_WHITE_BISHOP = (CX_WHITE | CX_BISHOP),
+    CX_WHITE_ROOK = (CX_WHITE | CX_ROOK),
+    CX_WHITE_QUEEN = (CX_WHITE | CX_QUEEN),
+    CX_WHITE_KING = (CX_WHITE | CX_KING),
+    CX_BLACK_PAWN = (CX_BLACK | CX_PAWN),
+    CX_BLACK_KNIGHT = (CX_BLACK | CX_KNIGHT),
+    CX_BLACK_BISHOP = (CX_BLACK | CX_BISHOP),
+    CX_BLACK_ROOK = (CX_BLACK | CX_ROOK),
+    CX_BLACK_QUEEN = (CX_BLACK | CX_QUEEN),
+    CX_BLACK_KING = (CX_BLACK | CX_KING),
+};
 
 enum {
     CX_CASTLE_WHITE_KING = CX_BIT << 0,
@@ -65,14 +84,20 @@ enum {
     CX_CASTLE_BLACK_QUEEN = CX_BIT << 3,
 };
 
-#define CX_WHITE_PIECES (CX_WHITE_PAWN | CX_WHITE_KNIGHT | CX_WHITE_BISHOP | CX_WHITE_ROOK | CX_WHITE_QUEEN | CX_WHITE_KING)
-#define CX_BLACK_PIECES (CX_BLACK_PAWN | CX_BLACK_KNIGHT | CX_BLACK_BISHOP | CX_BLACK_ROOK | CX_BLACK_QUEEN | CX_BLACK_KING)
-#define CX_ALL_PIECES (CX_WHITE_PIECES | CX_BLACK_PIECES)
-#define CX_EMPTY_SQUARES (~CX_ALL_PIECES)
+// Bitboard macros.
+#define CX_WHITE_PIECES(board) ((board)->pieces[CX_BB_WHITE_PAWNS] | (board)->pieces[CX_BB_WHITE_KNIGHTS] | (board)->pieces[CX_BB_WHITE_BISHOPS] | (board)->pieces[CX_BB_WHITE_ROOKS] | (board)->pieces[CX_BB_WHITE_QUEENS] | (board)->pieces[CX_BB_WHITE_KING])
+#define CX_BLACK_PIECES(board) ((board)->pieces[CX_BB_BLACK_PAWNS] | (board)->pieces[CX_BB_BLACK_KNIGHTS] | (board)->pieces[CX_BB_BLACK_BISHOPS] | (board)->pieces[CX_BB_BLACK_ROOKS] | (board)->pieces[CX_BB_BLACK_QUEENS] | (board)->pieces[CX_BB_BLACK_KING])
+#define CX_ALL_PIECES(board) (CX_WHITE_PIECES(board) | CX_BLACK_PIECES(board))
+#define CX_EMPTY_SQUARES(board) (~(CX_ALL_PIECES(board)))
 
+#define CX_PIECE_COLOR(piece) ((piece) & (CX_WHITE | CX_BLACK))
+#define CX_PIECE_TYPE(piece) ((piece) & (CX_PAWN | CX_KNIGHT | CX_BISHOP | CX_ROOK | CX_QUEEN | CX_KING))
+
+// Functions.
 cx_board_t *cx_board_init(void);
 void cx_board_free(cx_board_t *board);
 
+cx_piece_t cx_board_get_piece(cx_board_t const *board, uint8_t square);
 ssize_t cx_board_fen_load(cx_board_t *board, char const *fen);
 ssize_t cx_board_fen_dump(cx_board_t const *board, char *fen, size_t n);
 
