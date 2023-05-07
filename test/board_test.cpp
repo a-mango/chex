4df0b16ad13f889b53028002143c53b749da1484
acm <a-mango@users.noreply.github.com>
2023-05-07 17:07:40 +0200

    feat: extend board tests

--- test/board_test.cpp
+++ test/board_test.cpp
@@ -7,13 +7,35 @@
 extern "C" {
 #include "../src/util.h"
 #include "../src/board.h"
+#include "../src/print.h"
 }
 
-// Demonstrate some basic assertions.
 TEST(BoardTest, get_piece) {
     cx_board_t *board = cx_board_init();
     EXPECT_EQ(cx_board_get_piece(board, 0), CX_BLACK_ROOK);
     EXPECT_EQ(cx_board_get_piece(board, 63), CX_WHITE_ROOK);
+    EXPECT_EQ(cx_board_get_piece(board, 8), CX_BLACK_PAWN);
+    EXPECT_EQ(cx_board_get_piece(board, 48), CX_WHITE_PAWN);
+    EXPECT_EQ(cx_board_get_piece(board, 17), CX_EMPTY);
+
+    cx_board_free(board);
+}
+
+TEST(BoardTest, fen_load) {
+    cx_board_t *board = cx_board_init();
+    cx_board_fen_load(board, "k7/8/8/8/8/8/7/7K w - - 0 1");
+    cx_print_bitboard(board->pieces[CX_BB_BLACK_KING]);
+    EXPECT_EQ(cx_board_get_piece(board, 0), CX_BLACK_KING);
+    EXPECT_EQ(cx_board_get_piece(board, 63), CX_WHITE_KING);
+
+    cx_board_free(board);
+}
+
+TEST(BoardTest, fen_write) {
+    cx_board_t *board = cx_board_init();
+
+    char* fen = cx_board_fen(board);
+    EXPECT_EQ(fen, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w kqKQ - 0 1");
 
     cx_board_free(board);
 }
