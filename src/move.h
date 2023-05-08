15c68ee0d27abad44810b3d742a73aa6e27698a3
acm <a-mango@users.noreply.github.com>
2023-05-08 22:45:48 +0200

    chore: naming

--- src/move.h
+++ src/move.h
@@ -3,9 +3,9 @@
  * @see https://www.chessprogramming.org/Encoding_Moves
  * Created by acm on 5/6/23.
  *
- * From square (6 bits) - The starting square of the piece being moved.
- * To square (6 bits) - The ending square of the piece being moved.
- * Piece type (4 bits) - The type of the piece being moved.
+ * From square (7 bits) - The starting square of the piece being moved.
+ * To square (7 bits) - The ending square of the piece being moved.
+ * Piece (5 bits) - The type and color the piece being moved.
  * Promotion piece type (4 bits) - If the move is a promotion, the type of the piece being promoted to.
  * Capture (1 bit) - Whether the move captures an opponent's piece.
  * En passant (1 bit) - Whether the move is an en passant capture.
@@ -17,8 +17,8 @@
  *
  * Diagram:
  * msb                                            lsb
- * 000000  000000  0000   0000 0 0 00 0  0  0  000000
- * From    To      Piece  Prom C E Ca Ck Cm St Ep
+ * 0000000  0000000  00000  0000 0 0 00 0  0  0  000000
+ * From     To       Piece  Prom C E Ca Ck Cm St Ep
  */
 
 #ifndef CHEX_MOVE_H
@@ -32,17 +32,17 @@ typedef uint64_t cx_move_t;
 
 enum CxMove {
     CX_MOVE_NULL                 = 0,
-    CX_MOVE_FROM                 = 0b0000000000000000000000000000000000111111,
-    CX_MOVE_TO                   = 0b0000000000000000000000000000111111000000,
-    CX_MOVE_PIECE_TYPE           = 0b1111000000000000,
-    CX_MOVE_PROMOTION_PIECE_TYPE = 0b11110000000000000000,
-    CX_MOVE_CAPTURE              = 0b100000000000000000000000,
-    CX_MOVE_EN_PASSANT           = 0b100000000000000000000000000000,
-    CX_MOVE_CASTLE               = 0b11000000000000000000000000000000,
-    CX_MOVE_CHECK                = 0b100000000000000000000000000000000,
-    CX_MOVE_CHECKMATE            = 0b1000000000000000000000000000000000,
-    CX_MOVE_STALEMATE            = 0b10000000000000000000000000000000000,
-    CX_MOVE_EP_SQUARE            = 0b11111100000000000000000000000000000000000,
+    CX_MOVE_FROM                 = 0b1111111,
+    CX_MOVE_TO                   = 0b11111110000000,
+    CX_MOVE_PIECE                = 0b1111100000000000000,
+    CX_MOVE_PROMOTION            = 0b11110000000000000000000,
+    CX_MOVE_CAPTURE              = 0b100000000000000000000000000,
+    CX_MOVE_EN_PASSANT           = 0b100000000000000000000000000000000,
+    CX_MOVE_CASTLE               = 0b11000000000000000000000000000000000,
+    CX_MOVE_CHECK                = 0b100000000000000000000000000000000000,
+    CX_MOVE_CHECKMATE            = 0b1000000000000000000000000000000000000,
+    CX_MOVE_STALEMATE            = 0b10000000000000000000000000000000000000,
+    CX_MOVE_EP_SQUARE            = 0b11111100000000000000000000000000000000000000,
 };
 
 void cx_move_init(void);
@@ -54,7 +54,7 @@ void cx_undo(cx_move_t move, cx_board_t *board);
 // Move generation.
 size_t cx_move_gen(cx_board_t *board, cx_move_t *moves);
 size_t cx_move_gen_legal(cx_board_t *board, cx_move_t *moves);
-size_t cx_move_gen_pseudo_legal(cx_board_t *board, cx_move_t *moves);
+size_t cx_move_gen_pseudo(cx_board_t *board, cx_move_t *moves);
 
 // Move parsing.
 cx_move_t cx_move_parse(const cx_board_t * board, char const *san_str);
