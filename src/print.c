973fe04f8c87896bf76c2c28aef6ec86d6fc503a
acm <a-mango@users.noreply.github.com>
2023-05-08 11:40:12 +0200

    fix: board being displayed upside-down

--- src/print.c
+++ src/print.c
@@ -2,11 +2,12 @@
 // Created by acm on 5/7/23.
 //
 
+#include "print.h"
+
 #include <locale.h>
 #include <stdarg.h>
 #include <stdio.h>
 
-#include "print.h"
 #include "util.h"
 
 // Display settings
@@ -15,25 +16,25 @@
 #define CX_PRIWCHAR "lc"
 
 // Eye candy
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   L"\x1b[32m"
-#define ANSI_COLOR_YELLOW  L"\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN L"\x1b[32m"
+#define ANSI_COLOR_YELLOW L"\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 #define DEFAULT_COLOR ANSI_COLOR_YELLOW
 
 static const wchar_t *PIECE_SYMBOLS = L".♟♞♝♜♛♚♙♘♗♖♕♔";
-static const wchar_t *FILE_SYMBOLS = L"abcdefgh";
-static const wchar_t *RANK_SYMBOLS = L"12345678";
+static const wchar_t *FILE_SYMBOLS  = L"abcdefgh";
+static const wchar_t *RANK_SYMBOLS  = L"12345678";
 
 void cx_print_init(void) {
     setlocale(LC_ALL, "en_US.UTF-8");
     cx_print(DEFAULT_STREAM, DEFAULT_COLOR);
 }
 
-void cx_print(FILE* stream, wchar_t const *fmt, ...) {
+void cx_print(FILE *stream, wchar_t const *fmt, ...) {
     va_list args;
     va_start(args, fmt);
     vfwprintf(stream, fmt, args);
@@ -46,19 +47,18 @@ void cx_print_board(cx_board_t const *board) {
     wchar_t buffer[8][9] = {0};
 
     // Iterate over the bitboards.
-    for(size_t i = 0; i < 13; ++i) {
+    for (size_t i = 0; i < 13; ++i) {
         cx_bitboard_t bb = board->pieces[i];
-        // Print pieces to buffer using PIECE_SYMBOLS
-        while (bb) {
-            size_t pos = cx_pop_lsb(&bb);
-            size_t rank = pos / 8;
-            size_t file = pos % 8;
-            buffer[rank][file] = (wchar_t)PIECE_SYMBOLS[i];
+        // Print pieces to buffer using PIECE_SYMBOLS starting with msb
+        for (size_t j = 0; j < CX_BOARD_SQUARE_CNT; ++j) {
+            if (j % 8 == 0)
+                wprintf(L"\n");
+            if (bb >> (63 - j) & 1)
+                buffer[j / 8][j % 8] = PIECE_SYMBOLS[i];
         }
     }
-    for(size_t i = 0; i < CX_BOARD_SIZE; ++i)
-        wprintf(L"%ls\n", buffer[i]);
-    wprintf(L"\n");
+    for (size_t i = 0; i < CX_BOARD_SIZE; ++i)
+        wprintf(L"%ls\n", buffer[i]);  // Don't forget to reverse the buffer
 }
 
 void cx_print_bin(cx_piece_t n) {
@@ -69,10 +69,12 @@ void cx_print_bin(cx_piece_t n) {
 }
 
 void cx_print_bitboard(cx_bitboard_t n) {
-    for(size_t i = 0; i < CX_BOARD_SIZE; ++i) {
-        for(size_t j = 0; j < CX_BOARD_SIZE; ++j) {
-            wprintf(L"%c", (n & (CX_BIT << (i * CX_BOARD_SIZE + j))) ? 'x' : '.');
-        }
-        wprintf(L"\n");
+    // Print the bitboard in square shape using x and . starting with msb.
+    for (int i = 0; i < 64; i++) {
+        if (i % 8 == 0)
+            wprintf(L"\n");
+        wchar_t symbol = (n >> (63 - i)) & 1 ? 'x' : '.';
+        wprintf(L"%c", symbol);
     }
+    wprintf(L"\n");
 }
